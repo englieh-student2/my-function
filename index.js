5c08085fbe1e3a715dc9fabf4fc9d6ff56baabb7
EC2 Default User <ec2-user@ip-172-31-26-200.ec2.internal>
2022-03-06 03:17:17 +0000

    Change Files15

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda and Github on Cloud9 with AWS command!"),
+    body: JSON.stringify("Hello from Lambda and Github on Cloud9 with AWS command unzip properly!"),
   }
   return response
 }
